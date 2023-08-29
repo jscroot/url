96651161f20250577f6960e3d9fd70b32f574a7e
Rolly Maulana Awangga <rolly@awang.ga>
2023-08-29 07:03:33 +0700

    Update croot.js

--- croot.js
+++ croot.js
@@ -1,3 +1,15 @@
+export function getProtocol(){
+    return window.location.protocol.slice(0,-1);
+}
+
+export function getHost(){
+    return window.location.host;
+}
+
+export function getPath(){
+    return window.location.pathname.substring(1);
+}
+
 export function getQueryString(){
     return new Proxy(new URLSearchParams(window.location.search), {
         get: (searchParams, prop) => searchParams.get(prop),
