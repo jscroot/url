e93e03db3c21d457813d413c68b2992099189876
Rolly Maulana Awangga <awangga@gmail.com>
2024-01-08 09:11:45 +0700

    add folderPath

--- croot.js
+++ croot.js
@@ -10,6 +10,18 @@ export function fullPath(){
     return window.location.pathname;
 }
 
+export function folderPath(){
+    let fpth=window.location.pathname;
+    let dotArray = fpth.split(".");
+    if (dotArray.length >= 1){
+        extension = dotArray.pop();
+        slashArray = dotArray[0].split("/");
+        filename = slashArray.pop();
+        fpth=fpth.replace(filename+"."+extension,"");
+    }
+    return fpth;
+}
+
 export function getPath(){
     return window.location.pathname.substring(1);
 }
